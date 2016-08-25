faa8b7036109441096f1975d08734ed2e9bb0d2e
liao dong <867185661@qq.com>
2016-08-25 23:48:05 +0800

    20160825

---
+++ lesson/lesson01/consoleTrace.js
@@ -0,0 +1,5 @@
+user = new Object();
+user.name="liao dong"
+user.age=18
+user.sex="man"
+console.trace(user)
---
+++ lesson/lesson01/repl.js
@@ -0,0 +1,26 @@
+foo = 'bar'
+foo
+3<2
+3>2>1
+true==1
+true===1
+user = new Object()
+user.name="liao dong"
+user.name
+user.age = 24
+user
+user.setSchool = function(school){user.school = school}
+user.getSchool = function(){return user.school}
+user
+user.setSchool('南康中学')
+user.getSchool()
+a=3
+_+8
+a
+['apple','banana','pear']
+_.length
+_
+_.toString()
+_
+_
+_.toString()
